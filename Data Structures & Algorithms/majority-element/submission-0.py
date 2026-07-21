9a7b24f9fb37bed27b60162659d33484108e6434
Heman Ho <160170541+Heman-Ho@users.noreply.github.com>
2026-07-20 22:18:34 -0700

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Keep track of a tuple that represents a candidate for the maj element and it's frequency
+        
+        
+        
+        # After traversing the entire array, the candidate must be the maj element
+
+        candidate = 0
+        freq = 0
+
+        for i, num in enumerate(nums):
+            # Break early if we know for sure that current candidate is the maj element
+            if len(nums) - i < freq:
+                return candidate
+                
+            # add 1 to it's freq if the candidate appears in the array
+            if num == candidate:
+                freq += 1
+
+            # If the candidate's freq is 0 and the current value != candidate, 
+            # then switch the candidate to the current value and set it's frequency to 1.
+            elif freq == 0:
+                candidate = num
+                freq = 1
+            # if another elemnent is present, decrement the current candidate's frequency
+            else:
+                freq -= 1
+            
+        return candidate
+
