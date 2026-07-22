6b341b205ec85d0156688fe4bcd73e54d968e925
Heman Ho <160170541+Heman-Ho@users.noreply.github.com>
2026-07-21 22:51:57 -0700

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        def visitIsland(r: int, c: int) -> int:
+            # If out of bounds or current is not unvisited land, we return 0
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or grid[r][c] != 1:
+                return 0
+
+            # We mark the island so that we don't visit it again
+            grid[r][c] = 0
+
+            return 1 + visitIsland(r+1, c) + visitIsland(r-1, c) + visitIsland(r, c+1) + visitIsland(r, c-1)
+
+        greatestArea = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    greatestArea = max(greatestArea, visitIsland(r,c))
+        
+        return greatestArea
