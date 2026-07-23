fe75ff9e266f7133c04bd87b00415bb0bbb04918
Heman Ho <160170541+Heman-Ho@users.noreply.github.com>
2026-07-22 23:02:22 -0700

    Add: islands-and-treasure - submission-6

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-6.py
@@ -0,0 +1,31 @@
+from collections import deque
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        INF = 2147483647
+        
+        # We run a BFS on every treasure chest in rounds
+        # If it's land update it to that round's level
+
+        coords = deque()
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == 0:
+                    coords.append((row, col))
+
+        print(coords)
+
+        while coords:
+            row, col = coords.popleft()
+            neighbors = [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]
+            for r, c in neighbors:
+                if r < 0 or r >= ROWS or c < 0 or c >= COLS or grid[r][c] != INF: 
+                    continue
+                grid[r][c] = grid[row][col] + 1
+                coords.append((r, c))
+     
+
+
+
+        
